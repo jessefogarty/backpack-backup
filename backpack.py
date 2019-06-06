fa01c8869202fffdde43ea33eccc5408300ddc8b
Jesse Fogarty <jesse@email.tg>
2019-06-06 18:04:34 -0400

    Minor code changes
    Reducing lines, created a relative path check.
    
    Turned into a function for modularity and future use.
    
    Branching next change for CLI integration

--- backpack.py
+++ backpack.py
@@ -2,24 +2,41 @@
 
 import os
 from distutils.dir_util import copy_tree
+import re
 from termcolor import cprint
 
-orig_dir = input(' Enter the directory you want backed up: ')
+def backup():
+    orig_dir = input(' Enter path of directory to backup: ')
 
-if os.path.exists(orig_dir) and os.path.isdir(orig_dir):
+    # Check for relative path (~) and expand
+    if re.search('~', orig_dir):
+        orig_dir = os.path.expanduser(orig_dir)
+    else:
+        orig_dir = os.path.abspath(orig_dir)
+
+    print(os.path.join(orig_dir, 'backup'))
+
+
+    # Check if path exists AND IS a directory
+    if os.path.exists(orig_dir) and os.path.isdir(orig_dir):
+        # Change working directory to orig_dir
         os.chdir(orig_dir)
-        abspath = os.getcwd()
-        files = os.listdir('.')
-        cprint(f' Directory: {abspath} \n Files: {files}', 'yellow')
 
-# New backup directory to be created
-new_dir = abspath + '/' + 'backup'
+        # New backup directory to be created
+        # TODO: Add check to see if exists! Dir must NOT exist
+        new_dir = os.path.join(orig_dir, 'backup')
 
-try:
-    copy_tree('.', new_dir)
-except:
-    cprint('Copy of original files failed.', red); pass
+        # Try to copy the dirtree entirely
+        try:
+            copy_tree('.', new_dir)
+        except:
+            cprint('Copy of original files failed.', red); pass
 
-os.chdir(new_dir)
-new_abspath = os.getcwd() ; new_files = os.listdir('.')
-cprint(f' Backup directory: {new_abspath} \n Backup Files: {new_files}', 'green')
+        os.chdir(new_dir)
+        new_files = os.listdir('.')
+        cprint(f' Successfully backed up: {orig_dir}! Details below. \n Backup directory: {new_dir} \n Backup Files: {new_files}', 'green')
+    else:
+        print(f'Cannot find directory: {orig_dir}')
+
+if __name__ == "__main__":
+    backup()
