04dd19d9808aedffc10f665681dfaac2fa7cc441
Jesse Fogarty <jesse@email.tg>
2019-06-08 17:17:01 -0400

    Project structure overhaul
    - backpack.py now acts as the run file containing a CLI.
    
    - Modules used within the CLI are now contained in utils.
    
    - Simple, but working.
    
    ! - Next is:
        a) move created /backup/ directory to a destination
        b) ask to delete (unecrypted) backup files after encryption
        c) Upload to local pip environment test command line use
        d) Upload to pip - likely postponed because I'm chicken-shit
    
    Test folder shows a run. Used full relative path ~/github/backpack-backup/test AND with test (same folder test).

--- backpack.py
+++ backpack.py
@@ -2,41 +2,24 @@
 
 import os
 from distutils.dir_util import copy_tree
-import re
 from termcolor import cprint
+from utils.encrypt import dir_encrypt
+from utils.backup import dir_backup
+import argparse
+from time import sleep
 
-def backup():
-    orig_dir = input(' Enter path of directory to backup: ')
-
-    # Check for relative path (~) and expand
-    if re.search('~', orig_dir):
-        orig_dir = os.path.expanduser(orig_dir)
-    else:
-        orig_dir = os.path.abspath(orig_dir)
-
-    print(os.path.join(orig_dir, 'backup'))
-
-
-    # Check if path exists AND IS a directory
-    if os.path.exists(orig_dir) and os.path.isdir(orig_dir):
-        # Change working directory to orig_dir
-        os.chdir(orig_dir)
-
-        # New backup directory to be created
-        # TODO: Add check to see if exists! Dir must NOT exist
-        new_dir = os.path.join(orig_dir, 'backup')
-
-        # Try to copy the dirtree entirely
-        try:
-            copy_tree('.', new_dir)
-        except:
-            cprint('Copy of original files failed.', red); pass
-
-        os.chdir(new_dir)
-        new_files = os.listdir('.')
-        cprint(f' Successfully backed up: {orig_dir}! Details below. \n Backup directory: {new_dir} \n Backup Files: {new_files}', 'green')
-    else:
-        print(f'Cannot find directory: {orig_dir}')
+def cli():
+    parser = argparse.ArgumentParser(description='Backpack, an easy way to backup a directory.')
+    parser.add_argument('path', help='Path to directory', metavar='<PATH>')
+    parser.add_argument('--encrypt', help='Encrypt the backup files', action='store_true')
+    args = parser.parse_args()
+    encrypt = args.encrypt
+    path = args.path
+    # After dir_backup cwd is changed to /backup/ created
+    dir_backup(path)
+    sleep(2)
+    if encrypt == True:
+        dir_encrypt()
 
 if __name__ == "__main__":
-    backup()
+    cli()
